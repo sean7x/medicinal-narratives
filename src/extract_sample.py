ef29a0d74b7b20a98a6df4fc1b7d0bbb6e5c2bcb
sean7x <sean7x@gmail.com>
2023-10-01 22:20:55 +0800

    Fix positional argument settings

--- src/extract_sample.py
+++ src/extract_sample.py
@@ -25,11 +25,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Extract a stratified sample from dataset.')
     parser.add_argument(
-        "input_path", type=str, required=True,
+        "--input_path", type=str, required=True,
         help="Path to the input dataset"
     )
     parser.add_argument(
-        "output_path", type=str, required=True,
+        "--output_path", type=str, required=True,
         help="Path to save the output sample"
     )
     parser.add_argument(
