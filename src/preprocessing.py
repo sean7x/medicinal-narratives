e6edbc1785e17fd46fc20adff50b56ec09e4a654
jingyeelai <jylai@umich.edu>
2023-10-06 16:29:35 +0200

    Now it runs preprocessing based on the choosen library.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,3 @@
-import spacy
 import pandas as pd
 import html
 
@@ -7,32 +6,36 @@ from tqdm import tqdm
 tqdm.pandas()
 
 
-def preprocess(input_path, output_path):
-    nlp = spacy.load('en_core_web_sm')
+def preprocess(input_path, output_path, lib):
+    if lib == 'spacy':
+        import spacy
+        nlp = spacy.load('en_core_web_sm')
 
-    if input_path.suffix == ".csv":
-        df = pd.read_csv(input_path)
-    elif input_path.suffix == '.jsonl':
-        df = pd.read_json(input_path, lines=True)
-    elif input_path.suffix == '.parquet':
-        df = pd.read_parquet(input_path)
-    elif input_path.suffix == '.feather':
-        df = pd.read_feather(input_path)
+        if input_path.suffix == ".csv":
+            df = pd.read_csv(input_path)
+        elif input_path.suffix == '.jsonl':
+            df = pd.read_json(input_path, lines=True)
+        elif input_path.suffix == '.parquet':
+            df = pd.read_parquet(input_path)
+        elif input_path.suffix == '.feather':
+            df = pd.read_feather(input_path)
+        else:
+            raise ValueError(f"Unknown file type: {input_path.suffix}")
+        
+        # Decode HTML entities back to original characters and remove whitespaces
+        df['review'] = df['review'].apply(html.unescape).str.replace(r'[\r\n\t]', '', regex=True).str.strip()
+
+        # Remove wrong condition values and keep the rows
+        df.loc[df.condition.notna() & df.condition.str.contains('users found this comment helpful'), 'condition'] = None
+        
+        # Generate lemmas for each token, remove stopwords and punctuations, and join back into a string
+        df['procd_review'] = df['review'].progress_apply(
+            #lambda x: ' '.join([token.lemma_ for token in nlp(x) if not token.is_stop and not token.is_punct])
+            lambda x: [token.lemma_ for token in nlp(x) if not token.is_stop and not token.is_punct]
+        )
     else:
-        raise ValueError(f"Unknown file type: {input_path.suffix}")
-    
-    # Decode HTML entities back to original characters and remove whitespaces
-    df['review'] = df['review'].apply(html.unescape).str.replace(r'[\r\n\t]', '', regex=True).str.strip()
-
-    # Remove wrong condition values and keep the rows
-    df.loc[df.condition.notna() & df.condition.str.contains('users found this comment helpful'), 'condition'] = None
-    
-    # Generate lemmas for each token, remove stopwords and punctuations, and join back into a string
-    df['procd_review'] = df['review'].progress_apply(
-        #lambda x: ' '.join([token.lemma_ for token in nlp(x) if not token.is_stop and not token.is_punct])
-        lambda x: [token.lemma_ for token in nlp(x) if not token.is_stop and not token.is_punct]
-    )
-
+        import nltk
+        print('nltk')
     df.to_csv(output_path, index=False)
 
 
@@ -48,6 +51,11 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output_path", type=str, required=True,
         help="Path to save the output preprocessed dataset"
+    )
+
+    parser.add_argument(
+        "--input_path", type=str, required=True,
+        help="prepocess based on the library"
     )
 
     args = parser.parse_args()
