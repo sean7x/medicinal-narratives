561a89887e042eb783e5042370c745eacc8a48e0
Xuan Tao <taoxuan@umich.edu>
2023-10-11 14:53:09 +0800

    Add clustering grid search

--- src/grid_search.py
+++ src/grid_search.py
@@ -3,44 +3,41 @@ import subprocess
 
 
 # Grid searches
-def feature_grid_search():
-    # Grid search parameters for word2vec
-    vector_sizes = [100, 200, 300]
-    window_sizes = [5, 10, 15]
-    min_word_counts = [1, 5, 10]
-    epochs = [5, 10, 15]
-    sgs = [0, 1]
+def cluster_grid_search():
+    # Grid search hyperparameters for clustering
+    num_clusterses = [5, 10, 15, 20, 25, 30]
+    epses = [0.5, 1, 2, 3, 4, 5]
+    min_sampleses = [2, 3, 4, 5, 6, 7]
 
-    print("Feature engineering parameter search: \n")
+    print("Clustering hyperparameter search: \n")
 
-    for vector_size, window, min_count, epoch, sg in itertools.product(vector_sizes, window_sizes, min_word_counts, epochs, sgs):
+    for num_clusters, eps, min_samples in itertools.product(num_clusterses, epses, min_sampleses):
         subprocess.run([
             "dvc", "exp", "run", "--queue",
-            "--set-param", "feature_engineering.vector_size={vector_size}",
-            "--set-param", "feature_engineering.window={window}",
-            "--set-param", "feature_engineering.min_count={min_count}",
-            "--set-param", "feature_engineering.epoch={epoch}",
-            "--set-param", "feature_engineering.sg={sg}",
+            "--set-param", "clustering_bert.num_clusters={num_clusters}",
+            "--set-param", "clustering_bert.eps={eps}",
+            "--set-param", "clustering_bert.min_samples={min_samples}",
+            "-m", f"Clustering with num_clusters: {num_clusters}, eps: {eps}, min_samples: {min_samples}",
         ])
-        print(f"""vector_size: {vector_size}, window: {window}, min_count: {min_count}, epoch: {epoch}, sg: {sg} \n
+        print(f"""num_clusters: {num_clusters}, eps: {eps}, min_samples: {min_samples} \n
               has been added to the DVC queue. \n""")
     
     print("Run `dvc exp run --run-all` to start the grid search.")
 
 
 def topic_grid_search():
-    # Grid search parameters topic modeling
-    num_topics = [10, 20, 30, 40, 50]
+    # Grid search hyperparameters for topic modeling
+    nums_topics = [10, 20, 30, 40, 50]
 
-    print("Topic modeling parameter search: \n")
+    print("Topic modeling hyperparameter search: \n")
 
-    for num_topic in num_topics:
+    for num_topics in nums_topics:
         subprocess.run([
             "dvc", "exp", "run", "--queue",
-            "--set-param", "topic_modeling_bow_tfidf.lda_num_topics={num_topic}",
-            "--set-param", "topic_modeling_bow_tfidf.nmf_num_topics={num_topic}",
+            "--set-param", "topic_modeling_bow_tfidf.num_topics={num_topics}",
+            "-m", f"Topic modeling with num_topics: {num_topics}",
         ])
-        print(f"""lda_num_topics: {num_topic}, nmf_num_topics: {num_topic} \n
+        print(f"""num_topics: {num_topics} \n
               has been added to the DVC queue. \n""")
     
     print("Run `dvc exp run --run-all` to start the grid search.")
@@ -50,12 +47,12 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Grid search")
-    parser.add_argument("--feature", action="store_true", help="Feature engineering grid search")
+    parser.add_argument("--cluster", action="store_true", help="Clustering grid search")
     parser.add_argument("--topic", action="store_true", help="Topic modeling grid search")
     args = parser.parse_args()
 
-    if args.feature:
-        feature_grid_search()
+    if args.cluster:
+        cluster_grid_search()
     elif args.topic:
         topic_grid_search()
     else:
